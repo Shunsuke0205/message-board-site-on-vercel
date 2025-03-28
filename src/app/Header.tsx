3f8d423d17334088fd0d5f690b8f10792e5f1e4a
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-28 23:30:29 +0900

    impl: icon and link to home

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="border-b border-gray-300">
+      <Link href="/">
+        <Image
+          src="/product_icon.png"
+          alt="Icon"
+          width={60}
+          height={60}
+          className="p-2"
+        />
+        <h1>えん</h1>
+      </Link>
+    </div>
   )
 }
 
