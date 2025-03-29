d1374e67b785a08c1c72ebef12054abba9376425
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-29 23:00:30 +0900

    impl: category

--- src/app/complain/ComplainCard.tsx
+++ src/app/complain/ComplainCard.tsx
@@ -9,6 +9,7 @@ export type ComplainType = {
   good: number;
   cheer: number;
   bad: number;
+  category: number;
 };
 
 type ComplainCardProps = {
@@ -16,9 +17,26 @@ type ComplainCardProps = {
 };
 
 const ComplainCard = ({ complain }: ComplainCardProps) => {
+  const repeatEmoji = (emoji: string, count: number) => {
+    if (count >= 1 && count <= 10) {
+      return (
+        <span>
+          {emoji.repeat(count)}
+        </span>
+      )
+    } else {
+      return (
+        <span>
+          {emoji} {count}
+        </span>
+      )
+    }
+  }
+
   return (
     <article
       key={complain.id}
+      className="mt-2 ml-2 border"
     >
       <h2>
         {complain.postedBy}
@@ -30,10 +48,13 @@ const ComplainCard = ({ complain }: ComplainCardProps) => {
         {new Date(complain.createdAt).toLocaleString()}
       </p>
       <div>
-        <span>{complain.tears} tears</span>
-        <span>{complain.good} good</span>
-        <span>{complain.cheer} cheer</span>
-        <span>{complain.bad} bad</span>
+        <span>{repeatEmoji("\u{1F622}", complain.tears)}</span>
+        <span>{repeatEmoji("\u{2764}", complain.good)}</span>
+        <span>{repeatEmoji("\u{1F4E3}", complain.cheer)}</span>
+        
+      </div>
+      <div>
+        {complain.bad} bad
       </div>
     </article>
   )
