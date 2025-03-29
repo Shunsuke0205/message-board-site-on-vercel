212820f8dec6d92de603e89dc2e99d859fb911db
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-29 11:20:38 +0900

    remove: supabase url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 
 export default function Home() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
   return (
     <div>
       <p>記事投稿ページ</p>
-      <p>Are the environment variables available?</p>
-      <div>
-        <p>{supabaseUrl != undefined ? supabaseUrl : "Shunsuke"}</p>
-        <p>{supabaseAnonKey != undefined ? supabaseAnonKey : "can you see me?"}</p>
-      </div>
     </div>
   );
 }
