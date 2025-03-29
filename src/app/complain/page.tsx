0b9d311b144c4eeee6900de92de56e366ea4b563
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-30 00:08:37 +0900

    impl: complain form

--- src/app/complain/page.tsx
+++ src/app/complain/page.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ComplainCardList from "./ComplainCardList"
+import ComplainPostForm from "./ComplainPostForm"
 
 const Complain = () => {
   return (
     <div>
+      <ComplainPostForm />
       <ComplainCardList />
     </div>
   )
