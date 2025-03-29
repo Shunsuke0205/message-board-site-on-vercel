b9e7d3c820bac3ec7f865cb15f79063ab96b5964
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-29 15:55:05 +0900

    impl: DM page is not ready

--- src/app/direct-message/page.tsx
+++ src/app/direct-message/page.tsx
@@ -2,7 +2,10 @@ import React from "react"
 
 const DM = () => {
   return (
-    <div>DM</div>
+    <div>
+      <h1>DM</h1>
+      <p>&#x1f6a7; DM機能はおいおい実装させていただきます。 &#x1f647;&#x1f3fb;&#x200d;&#x2640;&#xfe0f;</p>
+    </div>
   )
 }
 
