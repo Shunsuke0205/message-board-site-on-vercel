71f45e4e93bab7fce12e99c2d4af11385bae44db
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-29 10:43:36 +0900

    impl: show the environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 
 export default function Home() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
   return (
     <div>
       <p>記事投稿ページ</p>
+      <div>
+        <p>{supabaseUrl}</p>
+        <p>{supabaseAnonKey}</p>
+      </div>
     </div>
   );
 }
