1a0cc25bd1b8bc0a0f6f5a65bf6b2fb9fd078d92
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-30 09:01:29 +0900

    impl: complain post form. SUCCEED in add to Supabase!

--- src/app/complain/ComplainPostForm.tsx
+++ src/app/complain/ComplainPostForm.tsx
@@ -1,8 +1,81 @@
-import React from "react"
+"use client"
+
+import { createClient } from "@/utils/supabase/client";
+import React, { useEffect, useState } from "react"
+
 
 const ComplainPostForm = () => {
+  const [name, setName] = useState("");
+  const [body, setBody] = useState("");
+  let userId: string | null;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const supabase = createClient();
+      const { data, error } = await supabase.auth.getUser();
+      if (error || !data?.user) {
+        console.error("Error fetching user in ComplainPostForm:", error);
+        return;
+      } else {
+        setName(data.user.id);
+        userId = data.user.id;
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const supabase = createClient();
+    const { data, error } = await supabase
+      .from("complain")
+      .insert([{
+        posted_by: name,
+        text: body,
+        tears: 1,
+        good: 1,
+        cheer: 1,
+        bad: 0,
+      }]);
+    if (error) {
+      console.error("Error inserting complain:", error);
+    } else {
+      console.log("Complain inserted:", data);
+    }
+  };
   return (
-    <div>ComplainPostForm</div>
+    <form>
+      <div>
+        <label htmlFor="name">
+          表示名
+        </label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="body">
+          内容
+        </label>
+        <textarea
+          id="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className="cursor-pointer"
+      >
+        投稿する
+      </button>
+    </form>
   )
 }
 
