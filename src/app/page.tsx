6a38fa73fa9aeb52f31b2536d4dc1a5375d3f166
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-30 23:32:13 +0900

    impl: css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div className="flex flex-col mt-5 ml-8">
       <h1 className="text-xl">トップページ</h1>
-      <h2 className="text-lg">主な使い方</h2>
-      <h3 className="">
+      <h2 className="text-lg mt-3">主な使い方</h2>
+      <h3 className="mt-2">
         <Link href={"/posts"}>
           Posts：返信ができる投稿機能
         </Link >
@@ -15,7 +15,7 @@ export default function Home() {
         質問や相談をする掲示板として使っていただけます。
       </p>
 
-      <h3>
+      <h3 className="mt-2">
         <Link href={"/complain"}>
           Guchi：返信機能のない、悩みやグチを吐き出す機能
         </Link>
@@ -25,8 +25,13 @@ export default function Home() {
         あえて返信できないようにしています。
       </p>
 
-      <h2>予定している機能</h2>
-      <h3>
+      <h2 className="mt-8 text-lg">予定している機能</h2>
+      <h3 className="mt-2">サイト全体について</h3>
+      <ul>
+        <li>見た目の調整</li>
+        <li>ローディングページ</li>
+      </ul>
+      <h3 className="mt-2">
         <Link href={"/posts"}>
           Posts
         </Link>
@@ -38,7 +43,7 @@ export default function Home() {
         <li>スレッドページ</li>
         <li>カテゴリー</li>
       </ul>
-      <h3>
+      <h3 className="mt-2">
         <Link href={"/complain"}>
           Guchi
         </Link>
@@ -49,9 +54,9 @@ export default function Home() {
         <li>非表示</li>
         <li>カテゴリー</li>
       </ul>
-      <h3>DM機能</h3>
+      <h3 className="mt-2">DM機能</h3>
 
-      <h2>実装できた機能</h2>
+      <h2 className="mt-5 text-lg">実装できた機能</h2>
     </div>
   );
 }
