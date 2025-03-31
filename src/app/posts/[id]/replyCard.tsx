08e2254f8f6115e7e5cd75f0ee3a36b4d7385604
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-31 14:57:05 +0900

    impl: css

--- src/app/posts/[id]/replyCard.tsx
+++ src/app/posts/[id]/replyCard.tsx
@@ -35,9 +35,10 @@ const ReplyCard = ({ reply }: ReplyCardProps) => {
   return (
     <article
       key={reply.id}
+      className="border border-gray-300 mt-4 px-4 py-2 "
     >
-      <div>
-        <div>
+      <div className="flex justify-between items-start">
+        <div className="flex items-center">
           <span>
             <Image
               src="/anonymous_user_icon.png"
@@ -47,22 +48,22 @@ const ReplyCard = ({ reply }: ReplyCardProps) => {
               className="rounded-full bg-gray-100"
             />
           </span>
-          <span>
+          <span className="ml-3 text-lg font-bold">
             {reply.postedBy}
           </span>
-          <span>
-            {new Date(reply.createdAt).toLocaleString()}
-          </span>
         </div>
-        <div>
-          <p>
-            {reply.content}
-          </p>
-          <div>
-            <span>
-              {repeatEmoji("\u{2764}", reply.like)}
-            </span>
-          </div>
+        <span className="text-sm text-gray-500">
+          {new Date(reply.createdAt).toLocaleString()}
+        </span>
+      </div>
+      <div className="px-13 pb-2">
+        <p className="whitespace-pre-wrap mt-4">
+          {reply.content}
+        </p>
+        <div className="mt-2">
+          <span>
+            {repeatEmoji("\u{2764}", reply.like)}
+          </span>
         </div>
       </div>
     </article>
