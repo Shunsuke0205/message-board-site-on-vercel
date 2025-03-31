478cf6c4848ee43e7bed68e769c84829aa08da25
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-31 09:48:06 +0900

    impl: css

--- src/app/complain/page.tsx
+++ src/app/complain/page.tsx
@@ -4,7 +4,7 @@ import ComplainPostForm from "./ComplainPostForm"
 
 const Complain = () => {
   return (
-    <div>
+    <div className="flex flex-col items-start">
       <ComplainPostForm />
       <ComplainCardList />
     </div>
