93456129842e4b78c5249b793614a515a107c453
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-31 11:49:24 +0900

    impl: form validation

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -26,15 +26,24 @@ export async function login(formData: FormData) {
 }
 
 export async function signup(formData: FormData) {
-  const supabase = await createClient()
-
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
   }
+  if (!data.email) {
+    console.error("error: email is empty")
+    return;
+  } else if (!data.password) {
+    console.error("error: password is empty")
+    return;
+  } else if (data.password.length < 8) {
+    console.error("error: password is too short")
+    return;
+  }
 
+  const supabase = await createClient()
   const { error } = await supabase.auth.signUp(data)
 
   if (error) {
