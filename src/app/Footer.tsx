6aca0847e088a635e8db1628f0773b8aeb8ab7ba
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-04 23:19:25 +0900

    impl: empty donation and gift page

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -8,7 +8,16 @@ const Footer = () => {
       <div className="mt-4 text-center text-gray-500">
         &copy; 2025 Shunsuke HIRATA
       </div>
-      <div className="fixed md:max-w-screen-md md:mx-auto bottom-0 left-0 right-0 flex justify-around bg-white border-t border-gray-300 p-2">
+      <div 
+        className="
+          fixed bottom-0 left-0 right-0 
+          md:max-w-screen-md md:mx-auto 
+          flex justify-around 
+          bg-white 
+          border-t border-gray-300 
+          p-2
+          overflow-x-auto"
+      >
         <Link
           href="/posts"
           className="mt-1 ml-2 pt-2 pb-2 pl-4 pr-4 bg-gray-100 items-center rounded-lg"
@@ -21,6 +30,34 @@ const Footer = () => {
               height={40}
             />
             Posts
+          </div>
+        </Link>
+        <Link
+          href="/complain"
+          className="mt-1 ml-2 pt-2 pb-2 pl-4 pr-4 bg-gray-100 items-center rounded-lg"
+        >
+          <div className="text-center">
+            <Image
+              src="/galaxy_icon.png"
+              alt="Icon"
+              width={40}
+              height={40}
+            />
+            Guchi
+          </div>
+        </Link>
+        <Link
+          href="/direct-message"
+          className="mt-1 ml-2 pt-2 pb-2 pl-4 pr-4 bg-gray-100 items-center rounded-lg"
+        >
+          <div className="text-center">
+            <Image
+              src="/donation_not_ready.png"
+              alt="Icon"
+              width={40}
+              height={40}
+            />
+            寄付
           </div>
         </Link>
         <Link
@@ -38,19 +75,20 @@ const Footer = () => {
           </div>
         </Link>
         <Link
-          href="/complain"
+          href="/direct-message"
           className="mt-1 ml-2 pt-2 pb-2 pl-4 pr-4 bg-gray-100 items-center rounded-lg"
         >
           <div className="text-center">
             <Image
-              src="/galaxy_icon.png"
+              src="/gift_not_ready.png"
               alt="Icon"
               width={40}
               height={40}
             />
-            Guchi
+            おゆずり
           </div>
         </Link>
+
       </div>
     </footer>
   )
