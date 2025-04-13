2c34d461ff3d6f21fb8f11ba18d098b76a556b66
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-13 22:18:06 +0900

    bugfix: nickname typo. impl: css

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -4,7 +4,7 @@ import { createClient } from "@/utils/supabase/client"
 import React, { use, useEffect, useState } from "react"
 
 type UserProfileProps = {
-  nikename: string;
+  nickname: string;
   sex: string;
   selfIntro: string;
   icon: number;
@@ -46,17 +46,16 @@ const EditPage = () => {
       
       if (data) {
         const profile: UserProfileProps = {
-          nikename: data.nikename,
+          nickname: data.nickname,
           sex: data.sex,
           selfIntro: data.selfIntro,
           icon: data.icon,
         }
         
-        if (data.nikename === null || data.nikename === undefined) {
+        if (data.nickname === null || data.nickname === undefined) {
           setNickname("");
         } else {
-          console.log("data.nikename", data.nikename);
-          setNickname(data.nikename);
+          setNickname(data.nickname);
         }
         if (data.selfIntro === null || data.selfIntro === undefined) {
           setSelfIntro("");
@@ -102,13 +101,9 @@ const EditPage = () => {
     if (error) {
       console.error("Error updating user profile in EditPage:", error);
       alert("プロフィールの更新に失敗しました。");
-    } else {
-      console.log("Profile updated:", data);
     }
-
-
-
   }
+
 
   return (
     (userId === null) ? (
@@ -118,22 +113,26 @@ const EditPage = () => {
       </div>
     ) : (
       <div>
-        <p>
-          ニックネーム：{(userProfile?.nikename == null) ? "未設定" : userProfile?.nikename}
-        </p>
-        <p>
-          性別：{(userProfile?.sex == null) ? "未設定" : userProfile?.sex}
-        </p>
-        <p>
-          自己紹介：{userProfile?.selfIntro}
-        </p>
-        <p>
-          アイコン： {(userProfile?.icon === -1) ? "未設定" : userProfile?.icon}
-        </p>
-        <form>
+        <h1 className="mt-4 text-lg">現在のプロフィール</h1>
+        <div className="mt-2 px-4 py-2 border-2 border-gray-300 rounded-lg">
+          <p>
+            ニックネーム：{(userProfile?.nickname == null) ? "未設定" : userProfile?.nickname}
+          </p>
+          <p>
+            性別：{(userProfile?.sex == null) ? "未設定" : userProfile?.sex}
+          </p>
+          <p>
+            自己紹介：{userProfile?.selfIntro}
+          </p>
+          <p>
+            アイコン： {(userProfile?.icon === -1) ? "未設定" : userProfile?.icon}
+          </p>
+        </div>
+        <h1 className="mt-4 text-lg">新しいプロフィール</h1>
+        <form className="mt-2 px-4 py-2 border-2 border-gray-300 rounded-lg">
           <div>
             <label htmlFor="nickname">
-              ニックネーム:
+              ニックネーム：
               <input
                 type="text"
                 id="nickname"
@@ -141,12 +140,16 @@ const EditPage = () => {
                 value={nickname}
                 onChange={(e) => setNickname(e.target.value)}
                 placeholder="ニックネームを入力してください"
+                className="
+                  ml-2 pb-[2px] 
+                  border-b border-gray-500 
+                  focus:outline-none focus:border-b-2 focus:pb-[1px]"
               />
             </label>
           </div>
           <div>
             <label htmlFor="sex">
-              性別:
+              性別：
               <select
                 name="sex"
                 id="sex"
@@ -162,23 +165,25 @@ const EditPage = () => {
           </div>
           <div>
             <label htmlFor="selfIntro">
-              自己紹介:
+              自己紹介：
               <textarea
                 name="selfIntro"
                 id="selfIntro"
-                cols={30}
-                rows={10}
+                cols={28}
+                rows={5}
                 value={selfIntro}
                 onChange={(e) => setSelfIntro(e.target.value)}
                 defaultValue={userProfile?.selfIntro}
                 placeholder="自己紹介を入力してください"
+                className="px-2 py-1 border-2 border-gray-300 rounded-lg"
               ></textarea>
             </label>
           </div>
           <div>
-            <label htmlFor="icon">
-              アイコン:
-              <select
+            <label htmlFor="icon"
+              className="text-gray-500">
+              すみません、アイコンの変更は後日実装予定です。&#x1f647;&#x1f3fb;&#x200d;&#x2640;&#xfe0f;
+              {/* <select
                 name="icon"
                 id="icon"
                 value={icon}
@@ -189,12 +194,13 @@ const EditPage = () => {
                 <option value="2">アイコン2</option>
                 <option value="3">アイコン3</option>
                 <option value="4">アイコン4</option>
-              </select>
+              </select> */}
             </label>
           </div>
           <button
             type="submit"
             onClick={handleSubmit}
+            className="mt-3 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
           >
             プロフィールを更新する
           </button>
