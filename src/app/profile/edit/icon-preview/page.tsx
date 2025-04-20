bff5d4ba1b7d9a8557ae571d13ad4b9717b3183a
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-20 18:39:39 +0900

    impl: explanation and css

--- src/app/profile/edit/icon-preview/page.tsx
+++ src/app/profile/edit/icon-preview/page.tsx
@@ -4,10 +4,25 @@ import { iconDataList, IconData } from "../iconData";
 const IconPreviewPage = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">アイコン一覧</h1>
-      <div className="flex flex-wrap">
+      <h1 className="mt-4 text-xl ">アイコン一覧</h1>
+      <p className="mt-2">
+        アイコンは「
+        <a 
+          href="https://iconbu.com/" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 underline"
+        >
+          フリーイラスト・アイコン素材　フリーペンシル
+        </a>  
+        」様のアイコンを使用させていただいております。
+      </p>
+      <p className="mt-2 text-sm text-gray-500">
+        データ容量の節約のため、アイコンの設定はアップロードではなく選択していただく形とさせていただきます。
+      </p>
+      <div className="mt-3 flex flex-wrap">
         {iconDataList.map((iconData: IconData) => (
-          <div key={iconData.iconNumber} className="w-1/5 p-2">
+          <div key={iconData.iconNumber} className="mr-3 mt-2 w-1/5 p-2 border-2 border-gray-300 rounded-lg">
             <img
               src={`/${iconData.Directory}/${iconData.fileName}`}
               alt={iconData.iconName}
