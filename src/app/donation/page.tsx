ed19878775daec18b026881cc9c2dc81f031d518
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-22 22:02:58 +0900

    impl: /donation page

---
+++ src/app/donation/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const DM = () => {
+  return (
+    <div>
+      <h1>DM</h1>
+      <p>&#x1f6a7; 寄付・DM・おゆずり機能はおいおい実装させていただきます。 &#x1f647;&#x1f3fb;&#x200d;&#x2640;&#xfe0f;</p>
+    </div>
+  )
+}
+
+export default DM
