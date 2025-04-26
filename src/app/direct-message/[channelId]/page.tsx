82c6c981946f956efddc9889d0f1d64b0edff083
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-26 17:23:09 +0900

    impl: DM send and read

--- src/app/direct-message/[channelId]/page.tsx
+++ src/app/direct-message/[channelId]/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@/utils/supabase/server';
 import React from "react"
-import MessageForm from './MessageFrom';
+import MessageForm from "./MessageFrom";
 
 export default async function Page({
   params,
@@ -14,20 +14,6 @@ export default async function Page({
     console.error("Error fetching user from Supabase in DM:", clientError);
     return <div>ログインされていないのでメッセージを表示できませんでした。</div>;
   }
-
-  const { data: DMmessageData, error: DMmessageError } = await supabase
-    .from("direct_message")
-    .select("*")
-    .eq("channel_id", channelId)
-    .or(`user1.eq.${clientData.user.id},user2.eq.${clientData.user.id}`)
-    .order("created_at", { ascending: true });
-
-  if (DMmessageError) {
-    console.error("Error fetching DM messages from Supabase in DM:", DMmessageError);
-    // return <div>メッセージの取得に失敗しました。</div>;
-  }
-  console.log("DMmessageData", DMmessageData);
-
   const { data: channelData, error: channelError } = await supabase
     .from("channel")
     .select("user1, user2")
@@ -35,17 +21,43 @@ export default async function Page({
     .single();
   if (channelError || !channelData) {
     console.error("Error fetching channel data from Supabase in DM:", channelError);
-    // return <div>チャンネルの取得に失敗しました。</div>;
+    return <div>チャンネルの取得に失敗しました。</div>;
   }
-  console.log("channel id", channelId);
-  console.log("channelData", channelData);
+ 
+  const { data: DMmessageData, error: DMmessageError } = await supabase
+    .from("direct_message")
+    .select("*")
+    .eq("channel_id", channelId)
+    .or(`from.eq.${clientData.user.id},to.eq.${clientData.user.id}`)
+    .order("created_at", { ascending: false });
+
+  if (DMmessageError) {
+    console.error("Error fetching DM messages from Supabase in DM:", DMmessageError);
+    // return <div>メッセージの取得に失敗しました。</div>;
+  }
+  console.log("DMmessageData", DMmessageData);
+
   const targetId = channelData?.user1 === clientData.user.id ? channelData.user2 : channelData?.user1;
-  console.log("targetId", targetId);
 
   return (
     <div>
-      {channelId}
       <MessageForm channelId={channelId} targetId={targetId} />
+      {DMmessageData && DMmessageData.map((message) => {
+        const isClientFrom = (message.from === clientData.user.id);
+        return (
+          <div key={message.id}
+            className="p-3 border-2 rounded-lg"
+          >
+            <div>
+              {isClientFrom ? "自分" : "相手"}: {message.message}
+            </div>
+            <div>
+              {new Date(message.created_at).toLocaleString()}
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   )
 }
