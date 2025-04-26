fd59672eb6e2b28dcaeb6d69fde1e8da51603d37
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-26 20:26:10 +0900

    impl: fecth channel data from Supabase

--- src/app/direct-message/page.tsx
+++ src/app/direct-message/page.tsx
@@ -1,4 +1,5 @@
 import { createClient } from "@/utils/supabase/server"
+import Link from "next/link";
 import React from "react"
 
 const DM_List = async () => {
@@ -13,15 +14,41 @@ const DM_List = async () => {
     )
   }
 
+  console.log("client id", userData.user.id);
+
   const { data: channelData, error: channelError } = await supabase
     .from("channel")
-    .select("*")
-    .or(`user1.eq.${userData.user.id},user2.eq.${userData.user.id}`);
+    .select(`
+      *,
+      user1Profile:profile!channel_user1_fkey1 (
+        nickname,
+        icon
+      ),
+      user2Profile:profile!channel_user2_fkey1 (
+        nickname,
+        icon
+      )
+    `)
+    .or(`user1.eq.${userData.user.id},user2.eq.${userData.user.id}`)
+    .order("last_update_at", { ascending: false });
   console.log("channelData", channelData);
+  if (channelError) {
+    console.error("Error fetching channel data from Supabase in DM:", channelError);
+  }
 
   return (
     <div>
       <h1>DM</h1>
+      {channelData && channelData.map((channel) => {
+        const targetId = (channel.user1 === userData.user.id) ? channel.user2 : channel.user1;
+        return (
+          <div key={channel.id} >
+            <Link href={`/direct-message/${channel.id}`}>
+              
+            </Link>
+          </div>
+        )
+      })}
     </div>
   )
 }
