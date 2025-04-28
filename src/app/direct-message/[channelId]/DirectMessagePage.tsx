bf0f19f99cf9279dcc100cce3a0c931f7bf1a688
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-04-28 09:43:48 +0900

    impl: MessageList component

--- src/app/direct-message/[channelId]/DirectMessagePage.tsx
+++ src/app/direct-message/[channelId]/DirectMessagePage.tsx
@@ -1,14 +1,14 @@
 import { createClient } from "@/utils/supabase/server";
 import React, { Suspense } from "react"
 import MessageForm from "./MessageFrom";
-import LocalizedDate from "@/component/LocalTime";
 import TargetProfile from "./TargetProfile";
+import MessageList from "./MessageList";
 
-type MessageListProps = {
+type DirectMessagePageProps = {
   channelId: string;
 }
 
-const DirectMessagePage = async ({ channelId }: MessageListProps) => {
+const DirectMessagePage = async ({ channelId }: DirectMessagePageProps) => {
   const supabase = await createClient();
   const { data: clientData, error: clientError } = await supabase.auth.getUser();
   if (clientError || !clientData?.user) {
@@ -29,20 +29,7 @@ const DirectMessagePage = async ({ channelId }: MessageListProps) => {
     return <div>チャンネルの取得に失敗しました。</div>;
   }
  
-  const { data: DMmessageData, error: DMmessageError } = await supabase
-    .from("direct_message")
-    .select("*")
-    .eq("channel_id", channelId)
-    .or(`from.eq.${clientData.user.id},to.eq.${clientData.user.id}`)
-    .order("created_at", { ascending: false });
-
-  if (DMmessageError) {
-    console.error("Error fetching DM messages from Supabase in DM:", DMmessageError);
-    // return <div>メッセージの取得に失敗しました。</div>;
-  }
-
   const targetId = channelData?.user1 === clientData.user.id ? channelData.user2 : channelData?.user1;
-
  
   return (
     <div>
@@ -53,23 +40,8 @@ const DirectMessagePage = async ({ channelId }: MessageListProps) => {
         <MessageForm channelId={channelId} targetId={targetId} />
       </Suspense>
       <Suspense fallback={<p>表示しています・・・</p>}>
-        {DMmessageData && DMmessageData.map((message) => {
-          const isClientFrom = (message.from === clientData.user.id);
-          return (
-            <div key={message.id}
-              className={`mt-2 px-3 py-2 border-2 border-gray-300 rounded-lg
-                ${isClientFrom ? "bg-lime-200" : "bg-pink-200"}`}
-            >
-              <p className="whitespace-pre-wrap">
-                {message.message}
-              </p>
-              <div>
-                <LocalizedDate createdAt={message.created_at} />
-              </div>
-            </div>
-          )
-        })}
-     </Suspense>
+        <MessageList channelId={channelId} />
+      </Suspense>
     </div>
   )
 }
---
+++ src/app/direct-message/[channelId]/MessageList.tsx
@@ -0,0 +1,55 @@
+import LocalizedDate from "@/component/LocalTime";
+import { createClient } from "@/utils/supabase/server";
+import React from "react"
+
+type MessageListProps = {
+  channelId: string;
+}
+const MessageList = async ({ channelId }: MessageListProps) => {
+  const supabase = await createClient();
+  const { data: clientData, error: clientError } = await supabase.auth.getUser();
+  if (clientError || !clientData?.user) {
+    console.error("Error fetching user from Supabase in DM:", clientError);
+    return (
+      <div>
+        ログインされていないのでメッセージを表示できませんでした。
+      </div>
+    );
+  }
+
+  const { data: DMmessageData, error: DMmessageError } = await supabase
+    .from("direct_message")
+    .select("*")
+    .eq("channel_id", channelId)
+    .or(`from.eq.${clientData.user.id},to.eq.${clientData.user.id}`)
+    .order("created_at", { ascending: false });
+
+  if (DMmessageError) {
+    console.error("Error fetching DM messages from Supabase in DM:", DMmessageError);
+    return <div>メッセージの取得に失敗しました。</div>;
+  }
+
+
+  return (
+    <div>
+      {DMmessageData && DMmessageData.map((message) => {
+        const isClientFrom = (message.from === clientData.user.id);
+        return (
+          <div key={message.id}
+            className={`mt-2 px-3 py-2 border-2 border-gray-300 rounded-lg
+              ${isClientFrom ? "bg-lime-200" : "bg-pink-200"}`}
+          >
+            <p className="whitespace-pre-wrap">
+              {message.message}
+            </p>
+            <div>
+              <LocalizedDate createdAt={message.created_at} />
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default MessageList
