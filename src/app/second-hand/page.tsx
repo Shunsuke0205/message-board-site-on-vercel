de294f6f0992dcedc41792d7550817a2c04fbb3a
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-05-06 21:58:10 +0900

    impl: SecondHandPostList component

--- src/app/second-hand/page.tsx
+++ src/app/second-hand/page.tsx
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { Suspense } from "react"
 import ImageFetch from "./ImageFetch"
+import { createClient } from "@/utils/supabase/server"
+
+const SecondHandPostList = async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("second_hand")
+    .select("*")
+    .order("created_at", { ascending: false })
+    .limit(20);
+
+  return (
+    <div>
+      SecondHandPostList
+    </div>
+  )
+}
+
 
 const SecondHandPage = () => {
   return (
     <div>
-      <ImageFetch /> 
       SecondHandPage
+      <ImageFetch />
+      <Suspense fallback={<div>表示しています・・・</div>}>
+        <SecondHandPostList />
+      </Suspense>
     </div>
   )
 }
