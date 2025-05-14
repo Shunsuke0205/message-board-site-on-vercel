83cd8c008bb4a78ab695c78160d57c8f7c78356a
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-05-14 21:22:21 +0900

    fix: chatbot-ui --> Open WebUI

--- en.sh
+++ en.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-wmctrl -s 0
-cd ~/Documents/vercel/wakka-board-site-on-vercel;
+~/Documents/open-webui/my-launch.sh &
+cd ~/Documents/vercel/wakka-board-site-on-vercel
 code . &
 konsole --new-tab -e npm run dev &
 google-chrome-stable http://localhost:8000 --profile-directory="Profile 13" &
-sleep 1
-~/Documents/chatbot-ui/start-chat-research.sh;
 
