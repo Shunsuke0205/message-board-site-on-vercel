fc18c1ee74982565e7867227102cee7ad2c4f30c
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-05-28 07:31:18 +0900

    fix: remove to check because isOwner check is done on post card

--- src/app/posts/DeleteButton.tsx
+++ src/app/posts/DeleteButton.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { createClient } from "@/utils/supabase/client"
 import { PostCommonProps } from "@/utils/postType"
 
@@ -10,26 +10,10 @@ type DeleteButtonProps = {
 };
 
 const DeleteButton = ({ post, tableName } : DeleteButtonProps) => {
-  const [userId, setUserId] = useState<string | null>(null);
-  const supabase = createClient();
-
-  //useEffect and fetch the userId
-  useEffect(() => {
-    const fetchUserId = async () => {
-      const { data, error } = await supabase.auth.getUser();
-      if (error || !data?.user) {
-        console.error("Error fetching user in DeleteButton:", error);
-        return;
-      }
-      setUserId(data.user.id);
-      
-    }
-
-    fetchUserId();
-  }, [supabase]);
 
   const deleteHandler = async () => {
     // check if the user is logged in
+    const supabase = createClient();
     const { data: userData, error: userError } = await supabase.auth.getUser();
     if (userError || !userData?.user) {
       console.error("Error fetching user in DeleteButton:", userError);
@@ -37,16 +21,17 @@ const DeleteButton = ({ post, tableName } : DeleteButtonProps) => {
     }
 
     // check if the userId is the same as the post's postedBy
-    if (userId !== post.postedBy) {
+    if (userData.user.id !== post.postedBy) {
       alert("この投稿はあなたのものではありません。");
       return;
     }
+
     // delete the post
     const { error } = await supabase
       .from(tableName || "post")
       .update({ isDeleted: true })
       .eq("id", post.id)
-      .eq("postedBy", userId);
+      .eq("postedBy", userData.user.id);
     if (error) {
       console.error("Error deleting post in DeleteButton:", error);
       return;
@@ -55,20 +40,18 @@ const DeleteButton = ({ post, tableName } : DeleteButtonProps) => {
     }
   }
   return (
-    (userId !== post.postedBy) ? null : (
-      <div>
-        <button
-          className="
-            mt-3 px-4 py-1 
-            bg-red-400 
-            font-bold text-white 
-            rounded cursor-pointer"
-          onClick={deleteHandler}
-        >
-          投稿を消す
-        </button>
-      </div>
-    )
+    <div>
+      <button
+        className="
+          mt-3 px-4 py-1 
+          bg-red-400 
+          font-bold text-white 
+          rounded cursor-pointer"
+        onClick={deleteHandler}
+      >
+        投稿を消す
+      </button>
+    </div>
   )
 }
 
