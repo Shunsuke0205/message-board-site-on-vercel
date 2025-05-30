326d51d5a32924fd839136be43b307bfbff43a4d
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-05-30 23:19:13 +0900

    remove

--- en.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-~/Documents/open-webui/my-launch.sh &
-cd ~/Documents/vercel/wakka-board-site-on-vercel
-code . &
-konsole --new-tab -e npm run dev &
-google-chrome-stable http://localhost:8000 --profile-directory="Profile 13" &
-
