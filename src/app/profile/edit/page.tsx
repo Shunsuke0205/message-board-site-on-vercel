88536c499e9f076217005a88035ea3230a3df5b1
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2026-04-05 19:48:09 +0900

    remove: TestNotificationButton because it has a bug

--- src/app/profile/edit/page.tsx
+++ src/app/profile/edit/page.tsx
@@ -138,7 +138,7 @@ const EditPage = () => {
               initialPostNotificationEnabled={postNotificationEnabled}
               initialDMNotificationEnabled={dmNotificationEnabled}
             />
-            <TestNotificationButton />
+            {/* <TestNotificationButton /> */}
           </div>
         )}
 
